96ec990f4ac845287e84b407c7129f2b4c15c30c
Pavel Gordienko <guzmo@yandex.ru>
2024-10-27 22:59:28 +0300

    feat: add some types for pokemons

--- pokeapi/src/app/page.tsx
+++ pokeapi/src/app/page.tsx
@@ -2,31 +2,35 @@
 
 import { useCallback, useEffect, useState } from "react";
 
-interface Pokemon {
-  name: string;
-  url: string;
-  sprite?: string;
-  typeName?: string;
-}
-
 interface PokemonType {
   slot: number;
   type: {
     name: string;
+    url: string;
   };
 }
 
 interface PokemonDetails {
   sprites: {
-    front_default: string;
+    front_default: string | null;
   };
   types: PokemonType[];
 }
 
+interface Pokemon {
+  name: string;
+  url: string;
+  sprite?: string;
+  typeNames?: string[];
+}
+
 export default function Home() {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [allPokemon, setAllPokemon] = useState<Pokemon[]>([]);
   const [offset, setOffset] = useState<number>(0);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+
   const pokemonHeight = 180;
 
   const calculateInitialLimit = () => {
@@ -35,13 +39,34 @@ export default function Home() {
     return Math.ceil(limit / 10) * 10;
   };
 
+  const fetchAllPokemonNames = async () => {
+    try {
+      const responseCount = await fetch("https://pokeapi.co/api/v2/pokemon");
+      if (!responseCount.ok) throw new Error("Error fetching total count");
+      const dataCount = await responseCount.json();
+      const totalCount = dataCount.count;
+
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=${totalCount}`);
+      if (!response.ok) throw new Error("Error fetching response data");
+      const data = await response.json();
+
+      const allPokemon: { name: string; url: string }[] = data.results.map((pokemon: Pokemon) => ({
+        name: pokemon.name,
+        url: pokemon.url
+      }));
+
+      setAllPokemon(allPokemon);
+      console.log(allPokemon);
+    } catch (error) {
+      console.log("Error fetching all Pokemon names:", error);
+    }
+  };
+
   const fetchPokemons = async (limit: number, currentOffset: number) => {
     setIsLoading(true);
     try {
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${currentOffset}`);
-      if (!response.ok) {
-        throw new Error("Error fetching data");
-      }
+      if (!response.ok) throw new Error("Error fetching data");
       const data = await response.json();
 
       const pokemonsWithDetails = await Promise.all(
@@ -66,25 +91,24 @@ export default function Home() {
     }
   };
 
-  const fetchPokemonDetails = async (url: string): Promise<{ sprite: string | null; typeName: string | null }> => {
+  const fetchPokemonDetails = async (url: string): Promise<{ sprite: string | null; typeNames: string[] }> => {
     try {
       const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error("Error fetching pokemon details");
-      }
+      if (!response.ok) throw new Error("Error fetching pokemon details");
+
       const data: PokemonDetails = await response.json();
+      const sprite = data.sprites.front_default || "file.svg";
+      const typeNames = data.types.map((typeObj: PokemonType) => typeObj.type.name);
 
-      const sprite = data.sprites.front_default || 'file.svg';
-      const typeName = data.types[0]?.type.name || null;
-
-      return { sprite, typeName };
+      return { sprite, typeNames };
     } catch (error) {
       console.error("Error fetching pokemon details:", error);
-      return { sprite: 'file.svg', typeName: null };
+      return { sprite: "file.svg", typeNames: [] };
     }
   };
 
   useEffect(() => {
+    fetchAllPokemonNames();
     const initialLimit = calculateInitialLimit();
     fetchPokemons(initialLimit, 0);
   }, []);
@@ -106,15 +130,33 @@ export default function Home() {
     };
   }, [handleScroll]);
 
+  const filteredPokemons = pokemons.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="flex flex-col items-center gap-2.5">
-      {pokemons.map((pokemon) => (
-        <div className="p-10 border-b flex flex-col items-center" key={pokemon.url}>
-          <h2 className="text-4xl font-semibold capitalize text-gray-400">{pokemon.name}</h2>
-          {pokemon.sprite && <img src={pokemon.sprite} alt={pokemon.name} width='150px' />}
-          <div className="capitalize">Type: {pokemon.typeName}</div>
-        </div>
-      ))}
+<input
+  type="text"
+  placeholder="Search Pokemon"
+  className="p-3 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent transition duration-200 ease-in-out shadow-md hover:shadow-lg text-black"
+  value={searchTerm}
+  onChange={(e) => setSearchTerm(e.target.value)}
+/>
+
+      {filteredPokemons.length > 0 ? (
+        filteredPokemons.map((pokemon) => (
+          <div className="p-10 border-b flex flex-col items-center" key={pokemon.url}>
+            <h2 className="text-4xl font-semibold capitalize text-gray-400">{pokemon.name}</h2>
+            {pokemon.sprite && <img src={pokemon.sprite} alt={pokemon.name} width="250px" />}
+            <div className="text-2xl font-semibold capitalize text-gray-500">
+              Type: {pokemon.typeNames?.join(', ') || 'Unknown'}
+            </div>
+          </div>
+        ))
+      ) : (
+        <p>No Pokemon found</p>
+      )}
     </div>
   );
 }
